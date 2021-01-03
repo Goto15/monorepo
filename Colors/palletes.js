7f3f9b16031f90ded0f235ccfd6467970ec5faa5
GoTo15 <goto15pod@gmail.com>
2021-01-03 12:59:27 -0600

    Add color palletes

---
+++ Colors/palletes.js
@@ -0,0 +1,12 @@
+const blackAndWhite = {
+  white: '#ffffff',
+  black: '#000000'
+}
+
+const mondrian = {
+  blue: '#2034d6',
+  red: '#f60201',
+  yellow: '#fded01',
+  white: '#ffffff',
+  black: '#000000'
+}
